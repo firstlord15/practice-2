ace9896dfcb51b0a133854b9b40f1a21ceb10344
Ratmir <ratmir.yuldashev28@mail.ru>
2023-09-12 12:04:54 +0300

    some fix and update

--- main_intarface_flet.py
+++ main_intarface_flet.py
@@ -22,14 +22,11 @@ def check():
 
 
 def main(page: ft.Page):
+    # Насторойка страницы
     page.title = NAME_PROGRAMM
+    page.theme_mode = "Light"
     page.window_center()
     page.scroll = True
-    data_size = 80
-
-    col_items = 30
-    min_pages = 1
-    page.num_page = 1
 
     start = time.time()  # точка отсчета времени
     # Подключение к базе данных
@@ -42,12 +39,14 @@ def main(page: ft.Page):
 
     rows = cursor.fetchall()
 
+    # Переменые с данными
+    data_size = 50
+    col_items = 30
+    min_pages = 1
+    page.num_page = 1
     max_pages, remainder = divmod(len(rows), col_items)
     if remainder > 0:
         max_pages += 1
-
-    # # Получение списка названий столбцов
-    # column_names = [desc[0] for desc in cursor.description]
 
     # Получение списка уникальных IP-адресов
     unique_ips = set(row[2] for row in rows)
@@ -58,7 +57,6 @@ def main(page: ft.Page):
 
     # DataTable в странице Flet
     datatable = ft.DataTable(
-        width=1300,
         columns=[
             ft.DataColumn(ft.Text("ID")),
             ft.DataColumn(ft.Text("Title_status_log")),
@@ -72,18 +70,12 @@ def main(page: ft.Page):
 
     def FixSizeString(data):
         result = ""
-        
-        if not isinstance(data, str):
-            data = str(data)
-
-        if len(data) > data_size:
-            result = data[0:data_size-1] + "..."
-        else:
-            result = data
+        if not isinstance(data, str): data = str(data)
+        if len(data) > data_size: result = data[0:data_size-1] + "..."
+        else: result = data
         return result
 
     rows_list = []
-
     for cartej in rows:
         id = FixSizeString(cartej[0])
         title_status_log = FixSizeString(cartej[1])
@@ -101,9 +93,9 @@ def main(page: ft.Page):
                 ft.DataCell(ft.Text(str(request_path))),  # Request_path
                 ft.DataCell(ft.Text(str(status_code))),  # Status_code
                 ft.DataCell(ft.Text(str(response_size))),  # Response_size
-            ],
+            ]
         )
-
+        
         rows_list.append(row)
 
     def AppendRowsDatatable(dtable):
@@ -126,40 +118,81 @@ def main(page: ft.Page):
         AppendRowsDatatable(datatable)
 
 
-
     def prev(e):
         if not page.num_page <= min_pages:
             page.num_page -= 1
         AppendRowsDatatable(datatable)
 
-    # Dropdawn для уникальных IP
-    ip_dropdawn = ft.Dropdown(hint_text="Уникальный IP для фильтра", width=1000)
-    ip_dropdawn.options.append(ft.dropdown.Option("None"))
+    def check_page_input(input):
+        if input.value.strip() != '':
+            pages = int(input.value)
+        else:
+            input.focus()
+            pages = ''
+        return pages
 
-    for elenments in unique_ips:
-        ip_dropdawn.options.append(ft.dropdown.Option(elenments))
+    def select_page(e):
+        pages = check_page_input(page_input)
+        if pages != '':
+            if min_pages <= pages <= max_pages:
+                page.num_page = pages
+        AppendRowsDatatable(datatable)
+        
+
+    # Dropdawn для уникальных IP
+    # ip_dropdawn = ft.Dropdown(hint_text="Уникальный IP для фильтра", width=1000)
+    # ip_dropdawn.options.append(ft.dropdown.Option("None"))
+
+    # for elenments in unique_ips:
+    #     ip_dropdawn.options.append(ft.dropdown.Option(elenments))
 
     # Кнопки интерфейса
-    btn_date_sort = ft.FilledButton(text="Сортировка по дате", width=500)
-    btn_ip_sort = ft.ElevatedButton(text="Сортировка по IP", width=500)
-    activte = ft.ElevatedButton(text="Применить фильтр", width=500)
-    save_json_file = ft.ElevatedButton(text="Сохранить в json файле", width=500)
+    # btn_date_sort = ft.FilledButton(text="Сортировка по дате", width=500)
+    # btn_ip_sort = ft.ElevatedButton(text="Сортировка по IP", width=500)
+    # activte = ft.ElevatedButton(text="Применить фильтр", width=500)
+    # save_json_file = ft.ElevatedButton(text="Сохранить в json файле", width=500)
 
     # Кнопки для перехода 
-    prev_button = ft.FloatingActionButton(text="Назад", width=500, on_click=prev)
-    next_button = ft.FloatingActionButton(text="Вперед", width=500, on_click=next)
+    prev_button = ft.FloatingActionButton(text="Назад", width=400, on_click=prev, bgcolor=ft.colors.AMBER_300, shape=ft.RoundedRectangleBorder(radius=5))
+    next_button = ft.FloatingActionButton(text="Вперед", width=400, on_click=next, bgcolor=ft.colors.AMBER_300, shape=ft.RoundedRectangleBorder(radius=5))
+    page_input = ft.TextField(hint_text="Номер страницы", width=120, border=None, border_radius=5)
+    page_button_select = ft.FloatingActionButton(text="Потвердить", width=120, on_click=select_page, shape=ft.RoundedRectangleBorder(radius=5))
 
     # Добавления объектов на страницу в определенном порядке
-    centered_content = ft.Column(
+    main_container = ft.ResponsiveRow(
         [
-            ft.Row([datatable], alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row([prev_button, next_button], alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row([btn_ip_sort, btn_date_sort], alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row([activte, save_json_file], alignment=ft.MainAxisAlignment.CENTER),
-            ft.Row([ip_dropdawn], alignment=ft.MainAxisAlignment.CENTER)
+            ft.Row([datatable], alignment=ft.MainAxisAlignment.CENTER, wrap=True),
+            ft.Row([page_input, page_button_select], alignment=ft.MainAxisAlignment.CENTER, wrap=True),
+            ft.Row([prev_button, next_button], alignment=ft.MainAxisAlignment.CENTER, wrap=True),
+            # ft.Row([btn_ip_sort, btn_date_sort], alignment=ft.MainAxisAlignment.CENTER),
+            # ft.Row([activte, save_json_file], alignment=ft.MainAxisAlignment.CENTER),
+            # ft.Row([ip_dropdawn], alignment=ft.MainAxisAlignment.CENTER)
         ],
     )
-    page.add(centered_content) # Добавлении основного контейнера
+
+    tabbar = ft.Tabs(
+        selected_index=1,
+        animation_duration=300,
+        tabs=[
+            ft.Tab(
+                text="Tab 1",
+                content=ft.Text("This is Tab 1")
+            ),
+            ft.Tab(
+                tab_content=ft.Icon(ft.icons.SEARCH),
+                content=main_container,
+            ),
+            ft.Tab(
+                text="Tab 3",
+                icon=ft.icons.SETTINGS,
+                content=ft.Text("This is Tab 3"),
+            ),
+        ],
+        expand=1,
+    )
+    
+    
+    page.add(tabbar) # Добавлении основного контейнера
     end = time.time() - start  # Собственно время работы программы
     print("time:", end)  # Вывод времени
 
